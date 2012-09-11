4697a246c2d0bd17f4f23f2c799b1fb1b5969d73
anderslyman <anderslyman@gmail.com>
2012-09-11 13:47:47 -0600

    Update index.js
    Found this update online somewhere, seems to work better

--- index.js
+++ index.js
@@ -1,45 +1,76 @@
-jQuery.fn.file = function(fn) {
-	return this.each(function() {
-		var el = $(this);
-		var holder = $('<div></div>').appendTo(el).css({
-			position:'absolute',
-			overflow:'hidden',
-			'-moz-opacity':'0',
-			filter:'alpha(opacity: 0)',
-			opacity:'0',
-			zoom:'1',
-			width:el.outerWidth()+'px',
-			height:el.outerHeight()+'px',
-			'z-index':1
-		});	
+jQuery.fn.choose = function (f) {
+    $(this).bind('choose', f);
+};
 
-		var wid = 0;
-		var inp;
+jQuery.fn.file = function () {
+    return this.each(function () {
+        var btn = $(this);
+        var pos = btn.offset();
 
-		var addInput = function() {
-			var current = inp = holder.html('<input '+(window.FormData ? 'multiple ' : '')+'type="file" style="border:none; position:absolute">').find('input');
+        function update() {
+            pos = btn.offset();
+            file.css({
+                'top': pos.top,
+                'left': pos.left,
+                'width': btn.width(),
+                'height': btn.height()
+            });
+        }
 
-			wid = wid || current.width();
+        btn.mouseover(update);
 
-			current.change(function() {
-				current.unbind('change');
+        var hidden = $('<div></div>').css({
+            'display': 'none'
+        }).appendTo('body');
 
-				addInput();
-				fn(current[0]);
-			});
-		};
-		var position = function(e) {
-			holder.offset(el.offset());					
+        var file = $('<div><form></form></div>').appendTo('body').css({
+            'position': 'absolute',
+            'overflow': 'hidden',
+            '-moz-opacity': '0',
+            'filter': 'alpha(opacity: 0)',
+            'opacity': '0',
+            'z-index': '2'
+        });
 
-			if (e) {
-				inp.offset({left:e.pageX-wid+25, top:e.pageY-10});						
-			}
-		};
+        var form = file.find('form');
+        var input = form.find('input');
 
-		addInput();
+        function reset() {
+            var input = $('<input type="file" multiple>').appendTo(form);
+            input.change(function (e) {
+                input.unbind();
+                input.detach();
+                btn.trigger('choose', [input]);
+                reset();
+            });
+        };
 
-		el.mouseover(position);
-		el.mousemove(position);
-		position();		
-	});
+        reset();
+
+        function placer(e) {
+            form.css('margin-left', e.pageX - pos.left - offset.width);
+            form.css('margin-top', e.pageY - pos.top - offset.height + 3);
+        }
+
+        function redirect(name) {
+            file[name](function (e) {
+                btn.trigger(name);
+            });
+        }
+
+        file.mousemove(placer);
+        btn.mousemove(placer);
+
+        redirect('mouseover');
+        redirect('mouseout');
+        redirect('mousedown');
+        redirect('mouseup');
+
+        var offset = {
+            width: file.width() - 25,
+            height: file.height() / 2
+        };
+
+        update();
+    });
 };
